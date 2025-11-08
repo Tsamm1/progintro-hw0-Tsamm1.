88cf63ff37f5a844cdad06a80c00ee14a9aa6754
Tsamm1 <anastasistsam@gmail.com>
2025-11-08 18:20:18 +0200

    Add aliquot.c file

---
+++ aliquot/src/aliquot.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main(){
+        int num, m;
+        char ch; 
+        printf("Please give the number to start the aliquot sequence from:");
+         scanf("%d", &num);
+        printf("Provide the max aliquot length to look for (0 for unlimited):");
+        scanf("%d", &m);
+        printf("Do you want to print the full sequence ('f') or just the length ('l')?");
+        scanf(" %c", &ch);
+        int sum=0;
+            while(sum !=1);
+            { int i=1;  
+                 while()
+                 {
+
+
+
+       
+       
+       
+       
+                }
+            }
+        }
