e4fc26322f02cbb33fb66aaf3e691d580525bf38
Tsamm1 <anastasistsam@gmail.com>
2025-11-11 15:31:47 +0200

    νεο ονομα

--- aliquot/src/aliquot.c
+++ aliquot/src/aliquot.c
@@ -1,25 +1,68 @@
 #include <stdio.h>
-int main(){
-        int num, m;
+int main(void){
+        long long num, len;
+        long long limit = 1000000000000000LL;
         char ch; 
         printf("Please give the number to start the aliquot sequence from:");
-         scanf("%d", &num);
+         if (scanf("%lld", &num) !=1)
+         {return 1;}
         printf("Provide the max aliquot length to look for (0 for unlimited):");
-        scanf("%d", &m);
+        if (scanf("%lld", &len) !=1)
+        {return 1;}
         printf("Do you want to print the full sequence ('f') or just the length ('l')?");
         scanf(" %c", &ch);
-        int sum=0;
-            while(sum !=1);
-            { int i=1;  
-                 while()
-                 {
-
-
-
+        long long count = 0;
+        if(num<=0)
+        {
+            return 1;
+        }
+        if (ch == 'f')
+        {
+            printf("%lld\n", num);
+        }
        
-       
-       
-       
+        while(1)
+        {
+            if (len !=0 &&count >=len)
+            {
+                break;
+            }
+             long long sum=1;
+        if (num ==1)
+        {sum=0;}
+        else 
+        {
+            for (long long i=2;i*i<=num ;i++ )
+            {
+                if (num%i==0)
+                {
+                    sum += i;
+                    if(i!=num/i)
+                    {
+                    sum+=num/i;
+                    }
                 }
             }
         }
+    num=sum;
+    count++;
+    if (num>limit)
+    {
+         printf("Number exceeds maximum supported integer (%lld). Stopping.\n", limit);
+        return 1;
+    }
+    if (num==0 || num==1){
+        break;
+    }
+if (ch=='f')       
+       {
+        printf("%lld\n", num);
+       }
+    }
+    if (ch =='l')
+    {
+         printf("Length of aliquot sequence: %lld\n", count);
+    }
+              
+return 0;}
+       
